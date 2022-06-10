92e4c2e22f53fbe2b21dddd41181c0bd81b675a2
keithrsinclaire <keithr.sinclair@gmaile.com>
2022-06-10 10:52:42 +0200

    Fiddling with Cards in Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './index.css';
+import Card from 'react-bootstrap/Card';
+
+
+import material1 from './images/material1.jpg';
+
 
 
 function App() {
   return (
     <div className="App">
       <h1> &nbsp;Landing Page</h1>
+      <Card>
+        <Card.Img variant="top" src={material1} />
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
